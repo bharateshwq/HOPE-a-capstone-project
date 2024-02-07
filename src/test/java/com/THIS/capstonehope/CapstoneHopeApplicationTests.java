816d81baabed9ccdefde68a29f35696a99cb4845
bharateshwq <breditter@gmail.com>
2024-02-07 06:53:16 +0530

    Spring initialized, added Login with Github and Google

---
+++ src/test/java/com/THIS/capstonehope/CapstoneHopeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.THIS.capstonehope;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CapstoneHopeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
