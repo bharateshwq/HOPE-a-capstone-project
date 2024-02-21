48c44c0f406d89f25e4733757f013bbf5ef84b6f
Lovelyangel006 <ushasatwika@gmail.com>
2024-02-21 11:02:14 +0530

    added base html pages

---
+++ frontend/thymeFrontend/login.js
@@ -0,0 +1,12 @@
+const wrapper = document.quertSelector('.wrapper');
+const loginLink = document.querySelector('.login-link')
+const registerLink = document.querySelector('.register-link');
+
+registerLink.addEventListener('click', ()=> { 
+    wrapper.classList.add('active')
+});
+
+
+loginLink.addEventListener('click', ()=> { 
+    wrapper.classList.remove('active')
+});
