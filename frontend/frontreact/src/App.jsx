4056da2cd2da7e7e67d3cd658a86dec86727947d
bharateshwq <breditter@gmail.com>
2024-03-11 10:50:32 +0530

    initiating react+vite app

---
+++ frontend/frontreact/src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+
+
+// import { IconName } from "react-icons/tfi";
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  <>
+  
+   
+      
+    
+ 
+  </>
+  )
+}
+
+export default App
