f4e0cb2400771e96eaa6a352584ad8c68ebb5d6b
Pulkit Jauhari <160208851+JPulkit17@users.noreply.github.com>
2024-03-15 15:07:59 +0530

    Update MyProfile.jsx

--- frontend/frontreact/src/pages/MyProfile.jsx
+++ frontend/frontreact/src/pages/MyProfile.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
-import ProfileContent from '../components/ProfileContent';
-import ProfileDetails from '../components/ProfileDetails';
+
 
 function MyProfile() {
   return (
 <div className="flex flex-col justify-center items-center w-full h-screen mt-96">
-  <ProfileContent />
-  <ProfileDetails />
+
 </div>
 
   )
